43743048c414d6282b49fba3e0f6a7946988407f
Luis Azocar <72492144+Gideon0@users.noreply.github.com>
2020-11-03 19:21:25 +0100

    Add files via upload

---
+++ ETC/P04/aritm2_nop.s
@@ -0,0 +1,24 @@
+; Varias operaciones aritmeticas
+; R5 = R4 – R3 + R2
+; R6 = R4 + R1 – R3
+
+    .text
+
+start:
+    add  r1, r0, #10    ; r1 = 10
+    add  r2, r0, #20  ; r2 = 20
+    add  r3, r0, #30  ; r3 = 30
+    nop
+    nop
+    add  r4, r0, #40  ; r4 = 40
+    nop
+    nop
+    sub r5, r4, r3    ; r5 = r5 – r3
+    nop
+    nop
+    add r5, r5, r2    ; r5 = r5 + r2
+    add r6, r4, r1    ; r6 = r4 + r1
+    nop
+    nop
+    sub r6, r6, r3    ; r6 = r6 – r3
+    trap #0       ; Fin del programa
