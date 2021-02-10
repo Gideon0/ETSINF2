f04eb3377a90ad923453b5f3041b5a3fd4d45eee
Ydav Păcat <72491654+YdavPacat@users.noreply.github.com>
2021-02-10 18:42:36 +0100

    Update Ejercicio4.java

--- RED/Pract7/Ejercicio4.java
+++ RED/Pract7/Ejercicio4.java
@@ -7,7 +7,7 @@ public class Ejercicio4
 {
    public static void main (String[] args)throws IOException, SocketException{
         DatagramSocket socket = new DatagramSocket();
-        String ms = new String("Ydav Pacat");
+        String ms = new String("Ydav Pacat\n");
         DatagramPacket dp = new DatagramPacket(ms.getBytes(),ms.getBytes().length, InetAddress.getByName("localhost"),7777);
         socket.send(dp);
         System.out.println("enviado");
