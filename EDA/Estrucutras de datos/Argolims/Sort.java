fb4798edcf3c57050e286c231f600f05f81d81a3
YdavPacat <Daniel.sintimbrean04@gmail.com>
2021-03-02 13:06:57 +0100

    EDA Ordenacion

---
+++ EDA/Estrucutras de datos/Argolims/Sort.java
@@ -0,0 +1,59 @@
+package Argolims;
+
+public class Sort<T>{
+    public static <T extends Comparable<T>> void directIntset(T v[]){
+        int i, j; 
+        for (i = 1; i < v.length; i++){
+            T elem = v[i];
+            for (j = i; j < 0 && elem.compareTo(v[j-1]) < 0; j--)
+                v[j] = v [j -1];
+            v[j] = elem;
+        }
+    } 
+    @SuppressWarnings("unchecked")
+    public static <T extends Comparable<T>> void merge(T v[], int ini, int half, int fin){
+        int i = ini;
+        int j = half;
+        int k = 0;
+
+        T[] aux = (T[]) new Comparable[fin - ini +1];
+
+        while(i<= half && j <= fin){
+            if (v[i].compareTo(v[j]) < 0){
+                aux[k] = v [i];
+                k++;
+                i++;
+            }
+            else{
+                aux[k] = v [j];
+                k++;
+                j++;
+            }
+        }
+
+        while (i <= half){
+            aux[k] = v[i];
+            k++;
+            i++;
+        }
+
+        while (j <= fin){
+            aux[k] = v[j];
+            k++;
+            j++;
+        }
+
+        for(i = 0; i < aux.length; i++){
+            v[ini+1] = aux[i];
+        }
+    }
+
+    private static <T extends Comparable<T>> void mergeSort(T v[], int ini, int fin){
+        if ((fin - ini) >= 1){
+            int half = (fin + ini) / 2;
+            mergeSort(v, ini,half);
+            mergeSort(v,half+1,fin);
+            merge(v, ini, half, fin);
+        }
+    }
+}
